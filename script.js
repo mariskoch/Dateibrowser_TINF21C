65215df6543d486151cc34f1ebfbb45800c2381f
mariskoch <maris@koch-24.de>
2022-06-29 23:03:40 +0200

    initial commit; login and authorization

---
+++ script.js
@@ -0,0 +1,41 @@
+//TODO: identifiactionString in Session speichern
+let identificationString = "";
+
+function login() {
+    let formData = new FormData(document.getElementById("credentialForm"));
+    let req = new XMLHttpRequest();
+    req.open('POST', 'http://localhost:8080/login', true);
+    req.setRequestHeader("Accept", "application/json");
+    req.onreadystatechange = function () {
+        if (req.readyState === 4) {
+            let response = JSON.parse(req.responseText);
+            if (req.status === 200) {
+                document.getElementById("loginResult").innerText = " login success: " + response.token;
+                let identificationSetup = formData.get("username") + ":" + response.token;
+                identificationString = "Basic " + btoa(identificationSetup);
+            } else {
+                document.getElementById("loginResult").innerText = "login error: " + response.error;
+                identificationString = "";
+            }
+        }
+    }
+    req.send(formData);
+}
+
+function testAuthorization() {
+    let req = new XMLHttpRequest();
+    req.open('GET', 'http://localhost:8080/', true);
+    req.setRequestHeader("Accept", "application/json")
+    req.setRequestHeader("Authorization", identificationString);
+    req.onreadystatechange = function () {
+        if (req.readyState === 4) {
+            let response = JSON.parse(req.responseText);
+            if (req.status === 200) {
+                document.getElementById("authorizationResult").innerText = "authorization success";
+            } else {
+                document.getElementById("authorizationResult").innerText = "authorization error: " + response.error;
+            }
+        }
+    }
+    req.send();
+}
