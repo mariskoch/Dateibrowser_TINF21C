7b3bd70375a8a7d2a41989b903f5aa6b5185f2ba
mariskoch <maris@koch-24.de>
2022-06-30 17:44:22 +0200

    further work on file upload

--- files.js
+++ files.js
@@ -1,19 +1,29 @@
-function addFile() {
+function addFile(event) {
     let req = new XMLHttpRequest();
-    req.open('POST', 'http://localhost:8080/File1', true);
-    req.setRequestHeader("ContentType", "application/x-www-form-urlencoded");
+    req.open('POST', 'http://localhost:8080/test.txt', true);
+    req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     req.setRequestHeader("Accept", "application/json");
     req.setRequestHeader("Authorization", identificationString);
 
-    let content = "Hall test";
     let formData = new FormData();
-    formData.append("content", btoa(content));
-    if (req.readyState === 4) {
-        let response = JSON.parse(req.responseText);
-        if (req.status === 200) {
-            alert(req.responseText);
-        } else {
-            alert(req.responseText);
+
+    let fileReader = new FileReader();
+    fileReader.onload = function () {
+        let lines = fileReader.result;
+        formData.append("content", btoa(lines.toString()));
+    }
+    let input = event.target.files[0];
+    console.log("MIMEType: " + input.mimeType);
+    fileReader.readAsText(input);
+
+    req.onreadystatechange = function () {
+        if (req.readyState === 4) {
+            let response = JSON.parse(req.responseText);
+            if (req.status === 200) {
+                console.log(response);
+            } else {
+                console.log(response);
+            }
         }
     }
     req.send(formData);
@@ -25,12 +35,14 @@ function refresh() {
     req.open('GET', 'http://localhost:8080/', true);
     req.setRequestHeader("Accept", "application/json");
     req.setRequestHeader("Authorization", identificationString);
-    if (req.readyState === 4) {
-        let response = JSON.parse(req.responseText);
-        if (req.status === 200) {
-            alert(req.responseText);
-        } else {
-            alert(req.responseText);
+    req.onreadystatechange = function () {
+        if (req.readyState === 4) {
+            let response = JSON.parse(req.responseText);
+            if (req.status === 200) {
+                console.log(response)
+            } else {
+                console.log(response);
+            }
         }
     }
     req.send();
