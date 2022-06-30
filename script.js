63c1f073e11d8f50e5d9abe9c63b62fed2e7fdb2
mariskoch <maris@koch-24.de>
2022-06-30 13:24:50 +0200

    added logout test

--- script.js
+++ script.js
@@ -39,3 +39,21 @@ function testAuthorization() {
     }
     req.send();
 }
+
+function logout() {
+    let req = new XMLHttpRequest();
+    req.open('GET', 'http://localhost:8080/logout', true);
+    req.setRequestHeader("Accept", "application/json")
+    req.setRequestHeader("Authorization", identificationString);
+    req.onreadystatechange = function () {
+        if (req.readyState === 4) {
+            let response = JSON.parse(req.responseText);
+            if (req.status === 200) {
+                document.getElementById("logoutResult").innerText = "logout success: " + response.message;
+            } else {
+                document.getElementById("logoutResult").innerText = "logout error: " + response.error;
+            }
+        }
+    }
+    req.send();
+}
