8b07d858ac520e995c7e18b2c724156e0f380563
mariskoch <maris@koch-24.de>
2022-06-30 20:24:03 +0200

    further work on file upload pt2

--- files.js
+++ files.js
@@ -1,33 +1,40 @@
+function GetValFromDataURL(dataURL) {
+    return dataURL.match(/^data:(.+);base64,(.+)$/);
+}
+
+function readyStateChangeFunction(req) {
+    if (req.readyState === 4) {
+        let response = JSON.parse(req.responseText);
+        if (req.status === 200) {
+            console.log(response);
+        } else {
+            console.log(response);
+        }
+    }
+}
+
 function addFile(event) {
+    let fileReader = new FileReader();
+    let formData = new FormData();
+    fileReader.onload = function () {
+        let valFromDataURL = GetValFromDataURL(fileReader.result);
+        formData.append("type", valFromDataURL[1]);
+        formData.append("content", valFromDataURL[2]);
+    }
+    console.log(formData);
+    let input = event.target.files[0];
+    fileReader.readAsDataURL(input);
+
+    let url = "http://localhost:8080/" + input.name;
     let req = new XMLHttpRequest();
-    req.open('POST', 'http://localhost:8080/test.txt', true);
+    req.open('POST', url, true);
     req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     req.setRequestHeader("Accept", "application/json");
     req.setRequestHeader("Authorization", identificationString);
-
-    let formData = new FormData();
-
-    let fileReader = new FileReader();
-    fileReader.onload = function () {
-        let lines = fileReader.result;
-        formData.append("content", btoa(lines.toString()));
-    }
-    let input = event.target.files[0];
-    console.log("MIMEType: " + input.mimeType);
-    fileReader.readAsText(input);
-
-    req.onreadystatechange = function () {
-        if (req.readyState === 4) {
-            let response = JSON.parse(req.responseText);
-            if (req.status === 200) {
-                console.log(response);
-            } else {
-                console.log(response);
-            }
-        }
-    }
+    req.addEventListener("readystatechange", function () {
+        readyStateChangeFunction(req);
+    });
     req.send(formData);
-    refresh();
 }
 
 function refresh() {
@@ -35,15 +42,21 @@ function refresh() {
     req.open('GET', 'http://localhost:8080/', true);
     req.setRequestHeader("Accept", "application/json");
     req.setRequestHeader("Authorization", identificationString);
-    req.onreadystatechange = function () {
-        if (req.readyState === 4) {
-            let response = JSON.parse(req.responseText);
-            if (req.status === 200) {
-                console.log(response)
-            } else {
-                console.log(response);
-            }
-        }
-    }
+    req.addEventListener("readystatechange", function () {
+        readyStateChangeFunction(req);
+    });
+    req.send();
+}
+
+function deleteFile() {
+    let fileName = document.getElementById("fileToDelete").value;
+    let url = "http://localhost:8080/" + fileName;
+    let req = new XMLHttpRequest();
+    req.open('DELETE', url, true);
+    req.setRequestHeader("Accept", "application/json");
+    req.setRequestHeader("Authorization", identificationString);
+    req.addEventListener("readystatechange", function () {
+        readyStateChangeFunction(req);
+    });
     req.send();
 }
